d7cb5e692300f1f307d58016d6ea25ef921dc60d
t-dlsantos <thiagodlsantos9@gmail.com>
2023-12-09 18:13:50 -0300

    fix: error with validation of password

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ const password = document.querySelector("#password");
 const confirmPassword = document.querySelector('#confirm_password');
 const errorParagraph = document.querySelector('.error-paragraph');
 
+
+password.addEventListener('input', () => {
+    if (confirmPassword.value === password.value) {
+        errorParagraph.style.display = 'none';
+    } else {
+        errorParagraph.style.display = 'block';
+    }
+});
+
 confirmPassword.addEventListener('input', () => {
     if (confirmPassword.value === password.value) {
         errorParagraph.style.display = 'none';
